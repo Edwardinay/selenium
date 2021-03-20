2e2e3328c587a413054ae1aa75aa22ea25209fca
VINAY <VINAY@LAPTOP-117JHIHR>
2021-03-20 19:01:09 +0530

    second

---
+++ oopss/src/inheritance/Bt.java
@@ -0,0 +1,13 @@
+package inheritance;
+
+public class Bt extends Overloading {
+	public void classsname () {
+		System.out.println("a");
+	}
+	
+  public static void main (String[]args) {
+	  Bt d=new Bt();
+	  d.classsname();
+	  d.classsname();
+  }
+}
